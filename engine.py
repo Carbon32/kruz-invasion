4ff25642369ab4273094053d00b6030bd8b3572a
Carbon32 <contact.carbonofficial@gmail.com>
2021-12-31 13:40:46 +0100

    The engine is able to load levels now

--- engine.py
+++ engine.py
@@ -23,6 +23,47 @@ mixer.init()
 windowWidth = 0
 windowHeight = 0
 
+scrollThresh = 300
+screenScroll = 0
+backgroundScroll = 0
+
+level = 1
+levelRows = 16
+levelColumns = 150
+tileSize = 50
+engineTiles = 24
+
+# Sprite Groups: 
+bulletGroup = pygame.sprite.Group()
+grenadeGroup = pygame.sprite.Group()
+explosionGroup = pygame.sprite.Group()
+enemyGroup = pygame.sprite.Group()
+chemicalsGroup = pygame.sprite.Group()
+gamePickups = pygame.sprite.Group()
+gameObjects = pygame.sprite.Group()
+gameExits = pygame.sprite.Group()
+
+# Tiles: #
+
+allTiles = []
+for c in range(engineTiles):
+	image = pygame.image.load(f'assets/Tiles/{c}.png')
+	image = pygame.transform.scale(image, (tileSize, tileSize))
+	allTiles.append(image)
+
+# Levels: #
+
+worldData = []
+for r in range(levelRows):
+	row = [-1] * levelColumns
+	worldData.append(row)
+
+with open(f'levels/level{level}_data.csv', newline='') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',')
+	for x, row in enumerate(reader):
+		for y, tile in enumerate(row):
+			worldData[x][y] = int(tile)
+
 # Engine Window: #
 
 class Window():
@@ -53,5 +94,38 @@ class Window():
 	def limitFPS(self, fps : int):
 		self.fpsLimit.tick(fps)
 	
-	def setBackground(self, background : pygame.Surface, x : int, y : int):
-		self.engineWindow.blit(background, (x, y))
+	def setBackground(self, sky : pygame.Surface, mountain : pygame.Surface, trees : pygame.Surface, x : int, y : int):
+		global backgroundScroll, windowHeight
+		self.engineWindow.fill((125, 255, 255))
+		width = sky.get_width()
+		for x in range(10):
+			self.engineWindow.blit(sky, ((x * width) - backgroundScroll * 0.5, 0))
+			self.engineWindow.blit(mountain, ((x * width) - backgroundScroll * 0.7, windowHeight - mountain.get_height() - 300))
+			self.engineWindow.blit(trees, ((x * width) - backgroundScroll * 0.9, windowHeight - trees.get_height() - 150))
+			self.engineWindow.blit(trees, ((x * width) - backgroundScroll * 1, windowHeight - trees.get_height()))
+
+# World: #
+
+# World Class: #
+
+class World():
+	def __init__(self):
+		self.obstacleList = []
+
+	def processData(self, data : list):
+		self.levelLength = len(data[0])
+		for y, row in enumerate(data):
+			for x, t in enumerate(row):
+				if(t >= 0):
+					tile = allTiles[t]
+					tileRect = tile.get_rect()
+					tileRect.x = x * tileSize
+					tileRect.y = y * tileSize
+					tileData = (tile, tileRect)
+					if(t >= 0 and t <= 8):
+						self.obstacleList.append(tileData)
+
+	def draw(self, engineWindow):
+		for tile in self.obstacleList:
+			tile[1][0] += screenScroll
+			engineWindow.blit(tile[0], tile[1])
--- main.py
+++ main.py
@@ -9,14 +9,26 @@
 
 from engine import *
 
-# Window Creation: #
+# Game Assets: #
+
+mountain = pygame.image.load('assets/Background/Mountain.png')
+trees = pygame.image.load('assets/Background/Pines.png')
+sky = pygame.image.load('assets/Background/Sky.png')
+
+# Game Window: #
 
 window = Window(1024, 768, "Land Invasion:")
 window.init()
+
+# World: #
+world = World()
+world.processData(worldData)
 
 # Game Loop: #
 
 while(window.engineRunning):
 	window.limitFPS(60)
+	window.setBackground(sky, mountain, trees, 0, 0)
+	world.draw(window.engineWindow)
 	window.updateDisplay()
 window.quit()
